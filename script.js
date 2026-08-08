eb8f38cb740d889c36917e0de617e7e0b0373ab5
XxatomzZ <cam.372@outlook.com>
2026-08-08 12:12:18 +0100

    updated projects page to contain full screen images for projects with a text drop down, added JS to make navbar shrink as the user scrolls, added some placeholder images (to be replaced later)

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 console.log("✅ JavaScript is connected!");
+
+// navbar changing size when scrolling
+const navbar = document.getElementById("navbar");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.getElementById("navbar");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 600) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+});
+
+
+
+// cover images for projects page with text window opening below
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".project-title");
+
+    console.log(buttons.length);
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const info = button.parentElement.nextElementSibling;
+            document.querySelectorAll(".project-info").forEach(panel => {
+                if (panel !== info) {
+                    panel.classList.remove("open");
+                }
+            });
+            info.classList.toggle("open");
+            setTimeout(() => {
+                info.scrollIntoView({
+                    behavior: "smooth",
+                    block: "start"
+                });
+            }, 500);
+        });
+    });
+});
