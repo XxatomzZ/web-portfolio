7ddfbd63ee9eb58c1576f75ee7370c6730b6a3e4
XxatomzZ <cam.372@outlook.com>
2026-08-17 18:04:54 +0100

    updated the homepage to now include a scrolling image slideshow of 'feature projects' (which I may remove or edit later) and a current project section

--- script.js
+++ script.js
@@ -40,3 +40,75 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 });
+
+
+
+// scrolling projects slideshow on homepage
+document.addEventListener('DOMContentLoaded', () => {
+
+    const track = document.querySelector('.gallery-track');
+    const previousButton = document.querySelector('.gallery-prev');
+    const nextButton = document.querySelector('.gallery-next');
+
+    if (!track || !previousButton || !nextButton) {
+        return;
+    }
+
+    nextButton.addEventListener('click', () => {
+        const maxScroll = track.scrollWidth - track.clientWidth;
+        if (track.scrollLeft >= maxScroll - 5) {
+            // loop back to image 1
+            track.scrollTo({
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            // move to the next image
+            track.scrollBy({
+                left: track.clientWidth,
+                behavior: 'smooth'
+            });
+        }
+    });
+    // and similarly for previous button
+    previousButton.addEventListener('click', () => {
+    if (track.scrollLeft <= 5) {
+        track.scrollTo({
+            left: track.scrollWidth - track.clientWidth,
+            behavior: 'smooth'
+        });
+    } else {
+        track.scrollBy({
+            left: -track.clientWidth,
+            behavior: 'smooth'
+        });
+    }
+});
+});
+
+
+// current project content tabs (homepage)
+const tabs = document.querySelectorAll(".tab");
+const panels = document.querySelectorAll(".content-panel");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    // remove active state from all tabs
+    tabs.forEach(tab => {
+      tab.classList.remove("active");
+    });
+    // add active state to clicked tab
+    tab.classList.add("active");
+    // get the clicked panel
+    const tabName = tab.dataset.tab;
+    // hide all panels
+    panels.forEach(panel => {
+      panel.classList.remove("active");
+    });
+    // show only selected panel
+    document
+      .getElementById(tabName)
+      .classList.add("active");
+  });
+});
+
